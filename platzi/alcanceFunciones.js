e0dd34e38c5ccde586a9a9164f015eea05a9eb76
Emmanuel Lopez <maximino33@hotmail.com>
2021-03-21 23:31:33 -0600

    videos añadidos

---
+++ platzi/alcanceFunciones.js
@@ -0,0 +1,44 @@
+//ALCANCE GLOBLAL
+let maxo = {
+    nombre: 'Maxo',
+    apellido: "Licea",
+    edad: 21
+}
+
+
+let dario ={
+    nombre: 'Dario',
+    apellido: 'Vargas',
+    edad: '19'
+}
+
+
+function name(persona){
+    //ALCANCE LOCAL
+    console.log(persona.nombre.toUpperCase());
+
+    /*
+        DESESTRUCTURAR OBJETOS
+    */
+    let {nombre} = persona;
+    console.log(nombre);
+
+    for(let i in persona){
+        console.log(i);
+    }
+}
+/*
+name(maxo);
+name(dario);
+name({nombre:'Maluma'});
+*/
+name(maxo);
+
+function cumpleaños(persona) {
+    //Creamos un nuevo objeto modificando la propiedad persona.
+    return{
+        ...persona,
+            edad: persona.edad + 1
+    }
+}
+
---
+++ platzi/pruebas.js
@@ -0,0 +1,7 @@
+let maxo = {
+    nombre: "Maxo",
+    apellido:"Maluma",
+    edad:"21"
+}
+
+console.log(Object.keys(maxo))
