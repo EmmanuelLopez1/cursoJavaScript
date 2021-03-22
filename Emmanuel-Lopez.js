7680ddbab68560aa3a9a689e43d2e801891d58f6
Emmanuel Lopez <maximino33@hotmail.com>
2021-03-21 23:28:25 -0600

    tarea 2 creada

--- Emmanuel-Lopez.js
+++ Emmanuel-Lopez.js
@@ -1,39 +1,51 @@
-let video = document.getElementById("video");
-let reproducir = document.getElementById("boton");
-let boton = document.getElementById("boton");
+let estado = parseInt(prompt("¿Como te sientes el día de hoy?: 1 alegre, 2 triste, 3 dolido, 4 muy dolido y 5 buchon"));
+estadoEmocional(estado);
 
-let age, nombre, apellido;
-
-
-
-function reproduce(nombre){
-    boton.style.display = "block";
-    boton.style.width = "95%";
-    boton.innerHTML = `Felicidades ${nombre} eres todo un crack y por ello haz ganado esta recompenza`;
-    reproducir.addEventListener('click',()=>{
-        boton.innerHTML = `Alaba a Ricardo papi dios Milos`;
+function seleccionBoton(name, ruta) {
+    return document.getElementById(name).addEventListener("click",()=>{
+        let video = document.getElementById("video");
+        console.log(video)
+        video.setAttribute("src", ruta);
         video.style.display = "block";
-        video.play();
-    });
+        
+    });    
 }
 
-    nombre = prompt("ingresa tu nombre crack");
-    apellido = prompt("ingresa tu apellido pana");
-    nombre = nombre +" " + apellido;
-
-do {
-    age = parseInt(prompt("ingresa tu edad"));
-    if(age < 18 ){
-        alert("¿como que tu edad es menor a 18? Ingrese su edad real joton XD");
+function estadoEmocional(estado) {
+    switch (estado) {
+        case 1:
+            mostrarVideo("alegre", "Asi me gusta crack que este al 100 diario");
+            break;
+        case 2:
+            mostrarVideo("triste", "¿Como vas a estar triste? Aquie esta ricardo milos para alegra tu dia.");
+            break;
+        case 3: 
+            mostrarVideo("dolido", "Echele aqui lo acompaña el buen jose Alfredo jimenez en su pena.");
+            break;
+        case 4:
+            mostrarVideo("muyDolido", "Que mal viejo, no hay cancion que esta para pistear de dolor.");
+            break;
+        case 5:
+            mostrarVideo("buchon", "Subale a la musica pariente,  !FIERR0¡");
+            break;
+        default:
+            alert("Ingresa un valor valido nigga");
+            break;
     }
-    else if(age >= 18){
-        alert("muy bien crack adelante");
-        reproduce(nombre);
-    }
-    
-} while (age < 18);
+}
 
+function mostrarVideo(btn, leyenda) {
+    let boton = document.getElementsByClassName(btn);
+    console.log(boton[0])
+    boton[0].style.display = "block";
+    boton[0].innerHTML = leyenda;
+    boton[0].style.width = "auto";
+}
 
-
+let alegre = seleccionBoton("alegre", "./videos/partyRock.mp4");
+let triste = seleccionBoton("triste", "./videos/Recompensa.mp4");
+let dolido = seleccionBoton("dolido", "./videos/me canse de rogarle.mp4");
+let muyDolido = seleccionBoton("muy dolido", "./videos/me canse de rogarle.mp4");
+let buchon = seleccionBoton("buchon", "./videos/que caro estoy pagando.webm");
 
 
